4cadf7a9c09b5703a3c2fa70b54d8419cbf384bc
Alexey-t <support@uvviewsoft.com>
2019-01-31 17:47:03 +0300

    trim

--- __init__.py
+++ __init__.py
@@ -21,22 +21,22 @@ def get_indent(s):
         if s[i] not in (' ', '\t'):
             return s[:i]
     return ''
-            
+
 class Command:
-    
+
     def __init__(self):
 
         opt.color_error = ini_read(fn_config, 'op', 'color_error', opt.DEF_ERROR)
         self.color_error = _theme_item(opt.color_error)
-        
+
         opt.color_set = ini_read(fn_config, 'op', 'color_set', opt.DEF_SET)
         self.color_set = [_theme_item(i) for i in opt.color_set.split(',')]
-        
+
         opt.lexers = ini_read(fn_config, 'op', 'lexers', opt.DEF_LEXERS)
         opt.max_lines = int(ini_read(fn_config, 'op', 'max_lines', '2000'))
 
     def get_color(self, n):
-    
+
         return self.color_set[n%len(self.color_set)]
 
     def config(self):
@@ -46,70 +46,70 @@ class Command:
         ini_write(fn_config, 'op', 'color_set', opt.color_set)
         ini_write(fn_config, 'op', 'max_lines', str(opt.max_lines))
         file_open(fn_config)
-        
+
     def on_change_slow(self, ed_self):
-        
+
         self.work(ed_self)
-        
+
     def on_open(self, ed_self):
-    
+
         self.work(ed_self)
-        
+
     def work(self, ed):
-    
+
         lex = ed.get_prop(PROP_LEXER_FILE)
         if not lex:
             return
         if not ','+lex+',' in ','+opt.lexers+',':
             return
-            
+
         if ed.get_line_count()>opt.max_lines:
             return
-   
+
         tab_size = ed.get_prop(PROP_TAB_SIZE)
         tab_spaces = ed.get_prop(PROP_TAB_SPACES)
-    
+
         ed.attr(MARKERS_DELETE_BY_TAG, tag=MARKTAG)
-    
+
         lines = ed.get_text_all().splitlines()
         for (index, s) in enumerate(lines):
             indent = get_indent(s)
             if not indent:
                 continue
-            
+
             level = -1
             x = 0
-            
+
             while indent:
                 level += 1
                 if indent[0]=='\t':
-                    ed.attr(MARKERS_ADD, 
-                        x=x, 
-                        y=index, 
-                        len=1, 
-                        tag=MARKTAG, 
+                    ed.attr(MARKERS_ADD,
+                        x=x,
+                        y=index,
+                        len=1,
+                        tag=MARKTAG,
                         color_font=0,
                         color_bg=self.get_color(level),
                         )
                     indent = indent[1:]
                     x += 1
                 elif indent[:tab_size]==' '*tab_size:
-                    ed.attr(MARKERS_ADD, 
-                        x=x, 
-                        y=index, 
-                        len=tab_size, 
-                        tag=MARKTAG, 
+                    ed.attr(MARKERS_ADD,
+                        x=x,
+                        y=index,
+                        len=tab_size,
+                        tag=MARKTAG,
                         color_font=0,
                         color_bg=self.get_color(level),
                         )
                     indent = indent[tab_size:]
                     x += tab_size
                 else:
-                    ed.attr(MARKERS_ADD, 
-                        x=x, 
-                        y=index, 
-                        len=len(indent), 
-                        tag=MARKTAG, 
+                    ed.attr(MARKERS_ADD,
+                        x=x,
+                        y=index,
+                        len=len(indent),
+                        tag=MARKTAG,
                         color_font=0,
                         color_bg=self.color_error,
                         )
